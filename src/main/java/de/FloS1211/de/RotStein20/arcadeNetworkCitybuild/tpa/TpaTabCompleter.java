4eef237deafe2ff203a0a121242e403fea3cf337
sonnflor <sonnflor@icloud.com>
2026-06-17 19:11:42 +0200

    added sign and tpa tab

---
+++ src/main/java/de/FloS1211/de/RotStein20/arcadeNetworkCitybuild/tpa/TpaTabCompleter.java
@@ -0,0 +1,34 @@
+package de.FloS1211.de.RotStein20.arcadeNetworkCitybuild.tpa;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TpaTabCompleter implements TabCompleter {
+  @Override
+  public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] args) {
+    List<String> result = new ArrayList<>();
+    if (args.length == 1) {
+      for (String item : List.of("me_to","to_me")) {
+        if (item.toLowerCase().startsWith(args[0].toLowerCase())) {
+          result.add(item);
+        }
+      }
+    }
+    if (args.length == 2) {
+      for (Player player : Bukkit.getOnlinePlayers()) {
+        if (player.getName().toLowerCase().startsWith(args[0].toLowerCase())) {
+          result.add(player.getName());
+        }
+      }
+    }
+    return result;
+  }
+}

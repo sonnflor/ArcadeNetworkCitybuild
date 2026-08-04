33b8c4ebd0366ddce07ad6399f0e9033b3847011
sonnflor <sonnflor@icloud.com>
2026-08-04 22:12:41 +0200

    reset all namecolor changes because they didnt work

--- src/main/java/de/FloS1211/de/RotStein20/arcadeNetworkCitybuild/namecolor/NamecolorGuiButtonExecutor.java
+++ src/main/java/de/FloS1211/de/RotStein20/arcadeNetworkCitybuild/namecolor/NamecolorGuiButtonExecutor.java
@@ -2,28 +2,16 @@ package de.FloS1211.de.RotStein20.arcadeNetworkCitybuild.namecolor;
 
 import de.FloS1211.de.RotStein20.arcadeNetworkCitybuild.gui.Gui;
 import de.FloS1211.de.RotStein20.arcadeNetworkCitybuild.gui.GuiButtonExecutor;
-import de.FloS1211.de.RotStein20.arcadeNetworkCitybuild.rank.PlayerNameManager;
-import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.ClickType;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.Objects;
-
 public class NamecolorGuiButtonExecutor implements GuiButtonExecutor {
   @Override
   public void customAction(String buttonId, Gui gui, Player player, ClickType clickType) {
-    if (buttonId.equals("default")) {
-      player.sendMessage("§f[§aNamensfarben§f] §7Du hast deine Namensfarbe deaktiviert");
-      NamecolorManager.setNamecolor(player.getUniqueId().toString(),"default");
-    } else {
-      NamecolorManager.setNamecolor(player.getUniqueId().toString(),buttonId);
-      player.sendMessage(LegacyComponentSerializer.legacyAmpersand().deserialize("§f[§aNamensfarben§f] §7").append(NamecolorManager.getColoredName(buttonId,"Du hast die Namensfarbe " + NamecolorManager.namecolors.get(buttonId).name() + " aktiviert",true)));
-    }
-    PlayerNameManager.updatePlayerName(player.getUniqueId().toString());
-    player.closeInventory();
+
   }
 
   @Override
